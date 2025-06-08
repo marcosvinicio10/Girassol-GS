61e272df7b37b100a75ab2aed08f18199cc24589
marcosvinicio10 <marcosouzavini2018@gmail.com>
2025-06-08 11:58:53 -0300

    Uodates

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -66,7 +66,13 @@ class ProductCard extends HTMLElement {
                     <p class="product-description">${description}</p>
                     <div class="product-price">R$ ${parseFloat(price).toFixed(2)}</div>
                     <div class="product-actions">
-                        <button class="buy-btn">Comprar</button>
+                        <button class="button">
+                            <svg viewBox="0 0 16 16" class="bi bi-cart-check" height="24" width="24" xmlns="http://www.w3.org/2000/svg" fill="#fff">
+                                <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"></path>
+                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
+                            </svg>
+                            <p class="text">Comprar</p>
+                        </button>
                         <button class="cart-btn">Adicionar ao carrinho</button>
                         <div class="favorite">
                             <input type="checkbox" id="favorite-${id}" class="favorite-checkbox">
@@ -82,7 +88,7 @@ class ProductCard extends HTMLElement {
 
     setupEventListeners() {
         const card = this.shadowRoot.querySelector('.product-card');
-        const buyBtn = this.shadowRoot.querySelector('.buy-btn');
+        const buyBtn = this.shadowRoot.querySelector('.button');
         const cartBtn = this.shadowRoot.querySelector('.cart-btn');
         const productImage = this.shadowRoot.querySelector('.product-image');
         const favoriteCheckbox = this.shadowRoot.querySelector('.favorite-checkbox');
@@ -100,6 +106,14 @@ class ProductCard extends HTMLElement {
         // Evento para a imagem do produto
         productImage.addEventListener('click', () => {
             this.navigateToProduct();
+        });
+
+        // Evento para o card inteiro
+        card.addEventListener('click', (e) => {
+            // Não navegar se o clique foi em um botão ou no checkbox
+            if (!e.target.closest('.button') && !e.target.closest('.cart-btn') && !e.target.closest('.favorite-checkbox')) {
+                this.navigateToProduct();
+            }
         });
 
         // Evento para o checkbox de favorito
